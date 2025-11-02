c9bcae046f06880bf1e1d252bfbe9b146a871700
Anime <franciscotadeuuu@gmail.com>
2025-11-02 18:58:36 -0300

    1

--- plantar/script.js
+++ plantar/script.js
@@ -48,15 +48,16 @@ else if (selectplantas === "tomate" && selectsolo === "argiloso") {
     const pont = document.getElementById("pontuação")
     const text = document.getElementById("text")
 
-    pont.innerText = temp_geral + chuva_geral+ "/100"
+    let final = temp_geral + chuva_geral
+    pont.innerText =  final 
 
-    if(Number(pont) >= 85 && Number(pont) <= 100){
+    if(Number(pont.innerText) >= 85 && Number(pont.innerText) <= 100){
         text.innerText  = "As condições estão ideais, ótimo momento para cultivar."
     }
-    else if(Number(pont) >= 60 && Number(pont) <= 84){
+    else if(Number(pont.innerText) >= 60 && Number(pont.innerText) <= 84){
         text.innerText  = "Condições razoáveis, com pequenos ajustes é possível plantar."
     }
-    else if(Number(pont) >= 35 && Number(pont) <= 59){
+    else if(Number(pont.innerText) >= 35 && Number(pont.innerText) <= 59){
         text.innerText  = "Condições medianas, talvez seja melhor adiar o plantio ou ajustar fatores."
     }
     else {
